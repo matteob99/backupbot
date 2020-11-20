a0b60809168cb63c7941ca58c473d1ceb17617b5
matteo <matteo.bocci@gmail.com>
2020-11-20 16:14:56 +0100

    fix name folder

--- backup-folder.py
+++ backup-folder.py
@@ -10,20 +10,20 @@ bot = Bot(TelegramAPI(api_key=getenv("TG_TOKEN_BACKUP"),
 
 date = dt.now().strftime("%Y_%m_%d_%H_%M")
 split = (f"/usr/bin/zip -r -s {getenv('MAX_SIZE_BACKUP','50m')} " +
-         f"date_{getenv('NAME')}.zip /data --password" +
+         f"{date}_{getenv('NAME')}.zip /data --password" +
          f" {getenv('BACKUP_PASSWORD')}")
 p = Popen(split, shell=True, stdout=PIPE)
 p.wait()
 chat = bot.chat(getenv("CHAT_BACKUP"))
-for i, file in enumerate(sorted(glob(f"date_{getenv('NAME')}.z*"),
+for i, file in enumerate(sorted(glob(f"{date}_{getenv('NAME')}.z*"),
                                 key=getmtime)):
     text = "#{custom}\n#d{date}\n{file}\nn:{list}".format(
         custom=getenv("NAME"),
         date=date,
-        file=f"date_{getenv('NAME')}",
+        file=f"{date}_{getenv('NAME')}",
         list=i
         )
     chat.send_file(path=file, caption=text)
-for file in glob(f"date_{getenv('NAME')}.z*"):
+for file in glob(f"{date}_{getenv('NAME')}.z*"):
     remove(file)
-print(f"finish backup date_{getenv('NAME')}")
+print(f"finish backup {date}_{getenv('NAME')}")
