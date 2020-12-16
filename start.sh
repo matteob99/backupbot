4c3595639b65a41121c474a62591ef9d501cb73a
matteo <matteo.bocci@gmail.com>
2020-12-16 17:04:29 +0100

    add github actions

--- start.sh
+++ start.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 printenv | sed 's/^\(.*\)$/export \1/g' > /code/env.sh
 chmod 0667 /code/env.sh
+if [ -n "$CRONTAB_CUSTOM" ]; then
+  sed 's/^[^root]*root//g' /etc/cron.d/backup-cron |  xargs echo "$CRONTAB_CUSTOM root" > tempfile
+  mv tempfile /etc/cron.d/backup-cron
+  crontab /etc/cron.d/backup-cron
+fi
 /usr/local/bin/python /code/backup.py
 crond -f
 
