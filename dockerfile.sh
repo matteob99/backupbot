53fa366741c8cbddff582f7155da6103ebcf5628
matteo <matteo.bocci@gmail.com>
2020-12-16 19:20:41 +0100

    add debug echo

--- dockerfile.sh
+++ dockerfile.sh
@@ -1,10 +1,14 @@
 #!/usr/bin/sh
 if [ "$database" = 'postgresql' ]; then
+  echo "apk add postgresql-client"
   apk add postgresql-client
 elif [ "$database" = 'mariadb' ]; then
+  echo "apk add mariadb-client"
   apk add mariadb-client
 elif [ "$database" = "redis" ]; then
+  echo "pip install redis"
   pip install redis
 elif [ "$database" = "influxdb" ]; then
+  echo "apk add influxdb"
   apk  add influxdb
 fi
